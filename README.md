2df5e78eb055fb766980a06b3cc8b13c66742e6c
terry.shi <shidakang@yundianjia.com>
2017-09-25 15:43:30 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # anginx
 
 ## simple parsing nginx log
+```
+log must use '|' to split
+for example '$remote_addr|$remote_port|$request_time|$remote_user|$time_local|$request|$status|$body_bytes_sent|$http_referer|$http_user_agent'
+```
--- anginx.go
+++ anginx.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
+	"os/exec"
+	"bytes"
 )
 
 // 配置文件结构体
@@ -19,6 +21,7 @@ type Conf struct {
 	Start_date string
 	End_date string
 	Overtime float64
+	Log_format string
 }
 
 var t Conf
@@ -28,7 +31,7 @@ var t Conf
 //	//os.Stdout.Write(line)
 //}
 //func ReadLine(filePth string, hookfn func([]byte)) error {
-func ReadLine(filePth string,result_path string) error {
+func ReadLine(filePth string,result_path string,ranged_key []string) error {
 	f, err := os.Open(filePth)
 	if err != nil {
 		return err
@@ -36,35 +39,73 @@ func ReadLine(filePth string,result_path string) error {
 	defer f.Close()
 	bfRd := bufio.NewReader(f)
 
+	//打开文件模式 文件不存在则创建 已有文件则清空 末尾添加 只写
 	result_f, err := os.OpenFile(result_path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY|os.O_APPEND, 0666)
 
 	for {
 		//按行读取文件
 		line, err := bfRd.ReadBytes('\n')
 		//hookfn(line) //放在错误处理前面，即使发生错误，也会处理已经读取到的数据。
-		// byte转string
-		str := string(line[:])
-		// string split
-		a := strings.Split(str," ")
-		// string转float
-		cost,_ := strconv.ParseFloat(a[4],3)
-		if cost > t.Overtime {
-			result_f.Write(line)
-		}
-		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
-			if err == io.EOF {
-				return nil
+
+		//空行判断
+		if len(line) != 0 {
+			var m = map[string]string{}
+			// byte转string
+			str := string(line[:])
+			// string split  类似于awk
+			a := strings.Split(str, "|")
+			index := 0
+			//值匹配 遍历map时key是随机化的 不能直接遍历
+			for _,key := range ranged_key {
+				m[key] = a[index]
+				index++
 			}
-			return err
+			//fmt.Println(m)
+			// string转float
+			cost, _ := strconv.ParseFloat(m["request_time"], 3)
+			if cost > t.Overtime {
+				result_f.Write(line)
+			}
+			if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
+				if err == io.EOF {
+					return nil
+				}
+				return err
+			}
+		} else {
+			return nil
 		}
 	}
 	return nil
 }
 
-func Sed(filePath string) {
-
+func FilterTime(start string,end string, file_path string) {
+	//build cmd
+	command := "sed -n '" + start + "," + end + "p' " + file_path + " > tmp"
+	// 使用bash才能够使用重定向 >
+	c := exec.Command("bash","-c",command)
+	//fmt.Println(c.Args)
+	// debug cmd
+	var stderr bytes.Buffer
+	c.Stderr = &stderr
+	err := c.Run()
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		return
+	}
 }
 
+func ParseFormat(format string) []string {
+	//创建map
+	str := strings.Split(format, "|")
+	ranged_key := make([]string,0)
+	for _,value := range str {
+		//删除prefix
+		a := strings.TrimPrefix(value,"$")
+		ranged_key = append(ranged_key,a)
+	}
+	return ranged_key
+}
 
 func main()  {
 	config_path := flag.String("c","","please offer config")
@@ -77,5 +118,7 @@ func main()  {
 	// 解析yaml文件
 	yaml.Unmarshal(config, &t)
 	fmt.Println("解析文件：",t.Input_file,"输出文件",t.Output_file)
-	ReadLine(t.Input_file,t.Output_file)
+	FilterTime(t.Start_date,t.End_date,t.Input_file)
+	ranged_key := ParseFormat(t.Log_format)
+	ReadLine("tmp",t.Output_file,ranged_key)
 }

89bf6bab723989b97aca576da4fee223d90da425
terry.shi <shidakang@yundianjia.com>
2017-12-01 15:27:56 +0800

    update

--- anginx_cron.sh
+++ anginx_cron.sh
@@ -5,7 +5,7 @@ echo $sed_time
 local_time=`date "+\/%d\\\\\\\\\\\/%b\\\\\\\\\\\/%Y\/"`
 echo $local_time
 str="startdate: $local_time"
-sed -n "/$sed_time/p" /etc/nginx/logs/www.liuyangbao.com_443.access.log > temp.log
+sed -n "/$sed_time/p" /etc/nginx/logs/access.log > temp.log
 sed -i "s/^.*startdate.*$/$str/" /root/anginx/anginx_config.yml
 /root/anginx/anginx-linux-amd64 -c /root/anginx/anginx_config.yml
 rm -f /root/Anginx_*
